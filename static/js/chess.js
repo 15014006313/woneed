3e165edcf66ea0d0b3560335a07bec1b132296b4
liu <1277843828@qq.com>
2020-12-17 19:45:55 +0800

    chess-v0.1

--- static/js/chess.js
+++ static/js/chess.js
@@ -1,52 +1,67 @@
-function Chess(arg) {
-    this.grid = arg.grid || [
-        ['A', 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 'B', 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    ];
-    this.curPlayer = true;
-    this.isAlive = true;
-    this.cellSize = 35;
-    this.boardSize = this.grid.length;
-    this.chessmanSize = 15;
-    this.player = { A: null, B: null };
-    this.Board = document.querySelector(arg.id);
-    this.Board.width = (this.boardSize + 1) * this.cellSize;
-    this.Board.height = (this.boardSize + 1) * this.cellSize;
-    this.drawBoard();
-}
-function Room(id) {
-    this.id = id;
-    this.player = [];
-}
-Chess.prototype = {
-    drawBoard: function () {
+class Chess {
+    constructor(arg) {
+        this.grid = arg.grid || [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 'B']
+        ];
+        this.curPlayer = true;
+        this.isAlive = true;
+        this.cellSize = 35;
+        this.boardSize = this.grid.length;
+        this.chessmanSize = 15;
+        this.player = { A: null, B: null };
+        this.Board = document.querySelector(arg.id);
+        this.Board.width = (this.boardSize + 1) * this.cellSize;
+        this.Board.height = (this.boardSize + 1) * this.cellSize;
+        this.dispatch = null;
+        this.isMe = false;
+        //绘画棋盘
+        this.drawBoard();
+        //添加事件监控
+        this.playHandle();
+    }
+    //重绘棋盘
+    reDrawBoard(grid = []) {
+        if (grid.length == 0) return
+        this.grid = grid;
+        this.context.clearRect(0, 0, this.Board.width, this.Board.height);
+        this.drawBoard();
+    }
+    //初始化棋盘
+    drawBoard() {
         this.context = this.Board.getContext("2d");
         for (var i = 0; i < this.boardSize; i++) {
             this.context.beginPath();
             this.context.moveTo(i * this.cellSize + this.cellSize, this.cellSize);
             this.context.lineTo(i * this.cellSize + this.cellSize, this.boardSize * this.cellSize);
-            
+
             this.context.moveTo(this.cellSize, i * this.cellSize + this.cellSize);
             this.context.lineTo(this.boardSize * this.cellSize, i * this.cellSize + this.cellSize);
             this.context.strokeStyle = "#dfdfdf";
             this.context.stroke();
             for (var j = 0; j < this.boardSize; j++) {
                 if (this.grid[i][j] != 0) {
-                    this.drawChess(i + 1, j + 1, this.grid[i][j] == 'A');
+                    this.drawChess(j + 1, i + 1, this.grid[i][j] == 'A');
                 }
             }
         }
+    }
+    //点击事件监听
+    playHandle() {
         this.Board.addEventListener("mousedown", (e) => {
             if (!this.isAlive) {
                 console.log("胜负已分！"); return;
+            }
+            if (!this.isMe) {
+                console.log("对手执棋"); return;
             }
             var cRect = this.Board.getBoundingClientRect();
             var canvasX = Math.round(e.clientX - cRect.left);
@@ -54,48 +69,37 @@ Chess.prototype = {
             var x = Math.floor(canvasX / this.cellSize), y = Math.floor(canvasY / this.cellSize);
             if (canvasX % this.cellSize < this.chessmanSize) {
                 if (canvasY % this.cellSize < this.chessmanSize) {
-                    this.step(x, y, this.curPlayer);
+                    this.checkStep(x, y, this.curPlayer);
                 } else if (this.cellSize - canvasY % this.cellSize < this.chessmanSize) {
-                    this.step(x, y + 1, this.curPlayer);
+                    this.checkStep(x, y + 1, this.curPlayer);
                 }
             } else if (this.cellSize - canvasX % this.cellSize < this.chessmanSize) {
                 if (canvasY % this.cellSize < this.chessmanSize) {
-                    this.step(x + 1, y, this.curPlayer);
+                    this.checkStep(x + 1, y, this.curPlayer);
                 } else if (this.cellSize - canvasY % this.cellSize < this.chessmanSize) {
-                    this.step(x + 1, y + 1, this.curPlayer);
+                    this.checkStep(x + 1, y + 1, this.curPlayer);
                 }
             }
         });
-    },
-    step: function (x, y, f) {
+    }
+    checkStep(x, y, f) {
         if (x == 0 || y == 0 || x == this.boardSize + 2 || y == this.boardSize + 2) {
             return false;
         }
         if (this.grid[y - 1][x - 1] != 0) {
             console.log("当前位置已落子"); return;
         }
-        this.context.beginPath();
-        this.context.arc(x * this.cellSize, y * this.cellSize, this.chessmanSize, 0, 2 * Math.PI);
-        this.context.closePath();
-        var gradient = this.context.createRadialGradient(x * this.cellSize + 2, y * this.cellSize - 2, this.chessmanSize, x * this.cellSize + 2, y * this.cellSize - 2, 0);
-        if (f) {
-            gradient.addColorStop(0, "#0A0A0A");
-            gradient.addColorStop(1, "#636766");
-        } else {
-            gradient.addColorStop(0, "#D1D1D1");
-            gradient.addColorStop(1, "#F9F9F9");
-        }
-        this.context.fillStyle = gradient;
-        this.context.fill();
+        //判断允许落子，绘画棋子
+        this.drawChess(x, y, f);
+        if (typeof this.dispatch === 'function')
+            this.dispatch({ x: y - 1, y: x - 1 });
+        this.isMe = false;
         this.grid[y - 1][x - 1] = this.curPlayer ? "A" : "B";
-        this.curPlayer = !this.curPlayer;
-        console.log(x, y);
+        //this.curPlayer = !this.curPlayer;
         this.checkBoard();
-    },
-    drawChess: function (x, y, f) {
-        if (x == 0 || y == 0 || x == this.boardSize + 2 || y == this.boardSize + 2) {
-            return false;
-        }
+    }
+    //绘画棋子
+    drawChess(x, y, f) {
         this.context.beginPath();
         this.context.arc(x * this.cellSize, y * this.cellSize, this.chessmanSize, 0, 2 * Math.PI);
         this.context.closePath();
@@ -109,8 +113,9 @@ Chess.prototype = {
         }
         this.context.fillStyle = gradient;
         this.context.fill();
-    },
-    checkBoard: function () {
+    }
+    //检测游戏是否结束
+    checkBoard() {
         for (var i = 0; i < this.grid.length; i++) {
             for (var j = 0; j < this.grid.length; j++) {
                 var row = i, col = j, rowflag = 1, colflag = 1, skewflag = 1, skewflag2 = 1;
@@ -165,3 +170,8 @@ Chess.prototype = {
         }
     }
 }
+function Room(id) {
+    this.id = id;
+    this.player = [];
+}
+
--- websocket/websocket-chess.js
+++ websocket/websocket-chess.js
@@ -1,42 +1,86 @@
 const { resolveNaptr } = require("mz/dns");
 const ws = require("nodejs-websocket");
-let chatUsers = [];
+let chessGame = {}, connTypeEnum = { userAddRoom: 'userAddRoom', serverInformation: 'serverInformation', chatterList: 'chatterList', gameStep: 'gameStep' };
+let initChess = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 var sever = ws.createServer(function (connect) {
     connect.on("text", function (str) {
         const data = JSON.parse(str);
         switch (data.type) {
-            case 'setName':
-                connect.type = 'addRoom';
-                connect.room = data.room;
-                connect.nickname = data.nickname;
-                boardcast({
+            case 'join':
+                if (chessGame[data.room] == undefined) {
+                    chessGame[data.room] = { alive: true, chessBoard: initChess, players: [], current: 0 };
+                }
+                if (chessGame[data.room].players.length < 2) {
+                    chessGame[data.room].players.push(data.id);
+                }
+                connect.info = {
+                    type: connTypeEnum.userAddRoom,
+                    id: data.id,
                     room: data.room,
-                    name: connect.nickname,
-                    type: 'serverInformation',
-                    message: data.nickname + "进入房间",
+                    nickname: data.nickname
+                };
+                boardcast({
+                    ...connect.info,
+                    current: chessGame[data.room].players[chessGame[data.room].current],
+                    message: data.nickname + "进入房间"
                 }, sever);
                 boardcast({
                     room: data.room,
-                    type: 'chatterList',
+                    type: connTypeEnum.chatterList,
                     list: getAllChatter(data.room, sever)
                 }, sever);
                 break;
-            case 'chat':
-                connect.type = 'sendMsg';
-                boardcast({
-                    type: 'chat',
-                    room: data.room,
-                    name: connect.nickname,
-                    message: data.message
-                }, sever);
-                break;
-            case 'hall':
-                connect.type = 'wechatHall';
-                boardcast({
-                    room: data.room,
-                    type: 'chatterList',
-                    list: getAllChatter('wechat_hall', sever)
-                }, sever);
+            case 'step':
+                connect.type = connTypeEnum.gameStep;
+                let cs = checkStep(data);
+                switch (cs) {
+                    case 0:
+                        //棋局已结束
+                        console.log("棋局已结束");
+                        break;
+                    case 1:
+                        //当前坐标易落子
+                        console.log("当前坐标易落子");
+                        break;
+                    case 2:
+                        //成功
+                        let res = {
+                            ...connect.info,
+                            message: '新落子'
+                        }, i = checkBoard(data.room);
+                        if (i == 0) {
+                            res.message = '新落子';
+                            res.current = chessGame[data.room].players[chessGame[data.room].current];
+                        } else if (i == 'A') {
+                            res.message = '游戏结束';
+                            res.winner = chessGame[data.room].players[0];
+                        } else if (i == 'B') {
+                            res.message = '游戏结束';
+                            res.winner = chessGame[data.room].players[1];
+                        }
+                        boardcast(res, sever); break
+                    case 3:
+                        //非选手无法操作
+                        console.log("非选手无法操作");
+                        break;
+                    case 4:
+                        //非当前选手执棋
+                        console.log("非当前选手执棋");
+                        break;
+                    default:
+                        break;
+                }
                 break;
             default:
                 break;
@@ -53,7 +97,7 @@ var sever = ws.createServer(function (connect) {
 
         //从在线聊天的人数上面除去
         boardcast(JSON.stringify({
-            type: 'chatterList',
+            type: connTypeEnum.chatterList,
             room: connect.room,
             list: getAllChatter(connect.room, sever)
         }), sever)
@@ -61,24 +105,95 @@ var sever = ws.createServer(function (connect) {
     connect.on('error', function (code) {
         console.log('异常：', code);
     });
-}).listen(3366);
-
+}).listen(7777);
+const checkStep = (data) => {
+    if (!chessGame[data.room].alive) return 0;
+    let players = chessGame[data.room].players,
+        current = chessGame[data.room].current,
+        chessBoard = chessGame[data.room].chessBoard;
+    if (players[current] != data.id) return 4;
+    let index = players.indexOf(data.id);
+    if (index > -1) {
+        if (chessBoard[data.x][data.y] != 0) {
+            console.log("当前位置已落子"); return 1;
+        }
+        chessBoard[data.x][data.y] = players.indexOf(data.id) == 0 ? 'A' : 'B';
+        chessGame[data.room].current = current == 0 ? 1 : 0;
+        return 2;
+    }
+    return 3;
+}
+const checkBoard = (room) => {
+    const grid = chessGame[room].chessBoard;
+    for (var i = 0; i < grid.length; i++) {
+        for (var j = 0; j < grid.length; j++) {
+            var row = i, col = j, rowflag = 1, colflag = 1, skewflag = 1, skewflag2 = 1;
+            if (grid[row][col] != 0) {
+                if (col <= grid.length - 4) {
+                    while (rowflag < 5) {
+                        //循环检测横向
+                        if (grid[row][col] == grid[row][col + rowflag]) {
+                            rowflag++;
+                        } else {
+                            rowflag = 99;
+                        }
+                    }
+                }
+                if (row <= grid.length - 4) {
+                    while (colflag < 5) {
+                        //循环检测竖向
+                        if (grid[row][col] == grid[row + colflag][col]) {
+                            colflag++;
+                        } else {
+                            colflag = 99;
+                        }
+                    }
+                    if (col <= grid.length - 4) {
+                        while (skewflag < 5) {
+                            //循环检测右斜向下
+                            if (grid[row][col] == grid[row + skewflag][col + skewflag]) {
+                                skewflag++;
+                            } else {
+                                skewflag = 99;
+                            }
+                        }
+                    }
+                }
+                if (row >= 4 && col <= grid.length - 4) {
+                    while (skewflag2 < 5) {
+                        //循环检测右斜向上
+                        if (grid[row][col] == grid[row - skewflag2][col + skewflag2]) {
+                            skewflag2++;
+                        } else {
+                            skewflag2 = 99;
+                        }
+                    }
+                }
+            }
+            if (rowflag == 5 || colflag == 5 || skewflag == 5 || skewflag2 == 5) {
+                chessGame[room].alive = false;
+                return grid[row][col];
+            }
+        }
+    }
+    return 0;
+}
+const isPlayer = (sever) => {
+    let players = sever.connections.filter(item => item.type != "watch");
+    return players.length < 2;
+}
 const boardcast = (str, sever) => {
     console.log(str);
     sever.connections.forEach((connect) => {
-        if (connect.type == 'wechatHall') {
+        if (connect.info.type == 'wechatHall') {
             connect.sendText(JSON.stringify({
                 type: 'chatterList',
                 list: getAllChatter('wechat_hall', sever)
             }))
-        }else{
-            if (str.room == connect.room) {
-                if (str.type == 'leave' && str.nickname != connect.nickname) {
-                    connect.sendText(JSON.stringify(str))
-                }
-                else {
-                    connect.sendText(JSON.stringify(str))
-                }
+        } else {
+            if (str.room == connect.info.room) {
+                let data = str.type == connTypeEnum.chatterList ? str : { ...str, chess: chessGame[str.room] }
+                connect.sendText(JSON.stringify(data));
             }
         }
     })
@@ -87,12 +202,12 @@ const getAllChatter = (room, sever) => {
     let chartterArr = [];
     sever.connections.forEach((connect) => {
         if (room == 'wechat_hall') {
-            if (connect.type == 'addRoom') {
-                chartterArr.push({ name: connect.nickname, room: connect.room })
+            if (connect.info.type == connTypeEnum.userAddRoom) {
+                chartterArr.push({ name: connect.info.nickname, room: connect.info.room })
             }
         } else {
-            if (room == connect.room) {
-                chartterArr.push({ name: connect.nickname, room: connect.room })
+            if (room == connect.info.room) {
+                chartterArr.push({ name: connect.info.nickname, room: connect.info.room })
             }
         }
     });
